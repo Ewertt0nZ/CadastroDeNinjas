6462d2e2278810daaabffed63c79e866d4ffcd84
Ewertt0nZ <ewerton0330@gmail.com>
2025-10-09 10:06:18 -0300

    feat: Coloca o Lombok na classe para abstracao

--- src/main/java/de/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/de/java10x/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -2,11 +2,17 @@ package de.java10x.CadastroDeNinjas.Missoes;
 
 import de.java10x.CadastroDeNinjas.Ninjas.Controller.Service.NinjaModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
--- src/main/java/de/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/de/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -3,11 +3,17 @@ package de.java10x.CadastroDeNinjas.Ninjas.Controller.Service;
 
 import de.java10x.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
 
 
@@ -17,7 +23,6 @@ public class NinjaModel {
     private String nome;
     private String email;
     private int idade;
-    private List<MissoesModel> missoesL;
 
     // @ManyToOne muitos ninjas tem uma unica missão
     @ManyToOne
@@ -26,37 +31,6 @@ public class NinjaModel {
 
 
 
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
 
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
