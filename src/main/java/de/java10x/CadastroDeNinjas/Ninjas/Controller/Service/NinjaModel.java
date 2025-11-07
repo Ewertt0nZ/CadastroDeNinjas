163570fa4a3f802044a3caa224b63b438d59d874
Ewertt0nZ <ewerton0330@gmail.com>
2025-11-07 10:28:10 -0300

    feat: Migrations para tb_cadastro incluindo a column rank

--- src/main/java/de/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
+++ src/main/java/de/java10x/CadastroDeNinjas/Ninjas/Controller/Service/NinjaModel.java
@@ -21,10 +21,14 @@ public class NinjaModel {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
     @Column(unique = true)
     private String email;
+
+    @Column(name = "img_Url")
+    private String imgUrl;
 
     @Column(name = "Idade")
     private int idade;
